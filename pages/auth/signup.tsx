5da25c96d88ef3807e142944f61c58caae4cc446
d5ng <dong5ffice@gmail.com>
2024-08-04 06:47:12 +0900

    ✨ Feat: Signup Page 추가

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,5 +1,10 @@
 import React from "react"
+import { SignupForm } from "@/features/auth/components"
 
 export default function SignupPage() {
-  return <div>SignupPage</div>
+  return (
+    <main className="w-screen h-screen flex justify-center items-center">
+      <SignupForm />
+    </main>
+  )
 }
