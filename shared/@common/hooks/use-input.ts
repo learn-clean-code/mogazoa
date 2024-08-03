0e0337d17490aa72d7124b428a42e9b93899bcd7
d5ng <dong5ffice@gmail.com>
2024-08-04 06:46:35 +0900

    ✨ Feat: useInput 훅 변경

--- shared/@common/hooks/use-input.ts
+++ shared/@common/hooks/use-input.ts
@@ -5,18 +5,23 @@ type Validation = (value: string) => boolean
 interface Props {
   validation?: Validation
   defaultValue?: string
+  formError?: boolean
+  onFormResetError?: () => void
 }
 
 export default function useInput(props: Props) {
   const [value, setValue] = useState(props.defaultValue || "")
   const [isTouched, setIsTouched] = useState(false)
 
-  const onInputChange: ChangeEventHandler<HTMLInputElement> = (event) => setValue(event.target.value)
+  const onInputChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setValue(event.target.value)
+    props.onFormResetError && props.onFormResetError()
+  }
   const onInputReset = () => setValue("")
   const onInputBlur = () => setIsTouched(true)
 
-  const isValid = props.validation && props.validation(value)
-  const hasError = !isValid && isTouched
+  const isValid = props.validation ? props.validation(value) : true
+  const hasError = props.formError ? true : !isValid && isTouched
 
   return {
     value,
