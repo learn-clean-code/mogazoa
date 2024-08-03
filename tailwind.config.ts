9574785733b05c38b9bfcd45920d89c54ba4b7ed
d5ng <dong5ffice@gmail.com>
2024-08-03 20:28:24 +0900

    🎨 Style: Project Color Setting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
@@ -8,13 +8,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "gray-50": "#F1F1F5",
+        "gray-100": "#9FA6B2",
+        "gray-200": "#6E6E82",
+        "gray-300": "#2E2E3A",
+        "gray-400": "#21212A",
+        "gray-500": "#17171C",
+        yellow: "#FFC83C",
+        green: "#05D58B",
+        pink: "#FF2F9F",
+        red: "#FF0000",
+        indigo: "#5363FF",
+        blue: "#5097FA",
+        background: "#1C1C22",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
