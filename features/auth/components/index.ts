2eff76e90e55a8c0bfac2395bcca397dbbac0b38
d5ng <dong5ffice@gmail.com>
2024-08-05 12:29:45 +0900

    🔥 Remove: 필요없는 import문 제거

--- features/auth/components/index.ts
+++ features/auth/components/index.ts
@@ -1,7 +1,3 @@
 import SignupForm from "./signup-form/signup-form"
-import SignupEmail from "./signup-form-control/signup-email"
-import SignupNickname from "./signup-form-control/signup-nickname"
-import SignupPassowrd from "./signup-form-control/signup-password"
-import SignupPasswordConfirm from "./signup-form-control/signup-password-confirm"
 
-export { SignupForm, SignupEmail, SignupNickname, SignupPassowrd, SignupPasswordConfirm }
+export { SignupForm }
