456be0888547424b5cd9d050c7d1c7b3311414bb
d5ng <dong5ffice@gmail.com>
2024-08-08 04:44:54 +0900

    ♻️ Refactor: signup-form 컴포넌트 리팩토링

--- features/auth/components/signup-form/signup-form.tsx
+++ features/auth/components/signup-form/signup-form.tsx
@@ -1,68 +1,36 @@
-import { Button, FormControl } from "@common/components"
-import { useForm } from "@common/hooks"
-import { AuthApiInstance } from "@/shared/@common/services"
-import { AxiosError } from "axios"
-import { validate, defaultValues } from "@/features/auth/helper"
+import { Button, Form } from "@common/components"
+import { validate, defaultValues, onSubmit } from "@/features/auth/logic"
 import type { DefaultValues } from "@/features/auth/types"
+import {
+  SignupFormEmail,
+  SignupFormNickname,
+  SignupFormPassword,
+  SignupFormPasswordConfirm,
+} from "@/features/auth/components"
 
 export default function SignupForm() {
-  const { register, handleSubmit, isFormError } = useForm<DefaultValues>({
-    defaultValues: defaultValues,
-    validate: validate,
-  })
-
-  const onSubmit = async (values: DefaultValues) => {
-    try {
-      await AuthApiInstance.signup({
-        email: values.email,
-        nickname: values.nickname,
-        password: values.password,
-        passwordConfirmation: values.passwordConfirm,
-      })
-    } catch (err) {
-      const error = err as AxiosError<{ message: string }, any>
-      throw { email: error.response!.data.message || "알 수 없는 에러가 발생했어요" }
-    }
-  }
-
   return (
-    <form className="w-[640px] flex flex-col gap-y-10" onSubmit={handleSubmit(onSubmit)}>
-      <FormControl {...register("email")} id="email">
-        <FormControl.Layout>
-          <FormControl.Label>이메일</FormControl.Label>
-          <FormControl.Input type="text" placeholder="이메일을 입력해주세요" />
-          <FormControl.ErrorMessage />
-        </FormControl.Layout>
-      </FormControl>
-      <FormControl {...register("nickname")} id="nickname">
-        <FormControl.Layout>
-          <FormControl.Label>닉네임</FormControl.Label>
-          <FormControl.Input type="text" placeholder="닉네임을 입력해주세요" />
-          <FormControl.ErrorMessage />
-        </FormControl.Layout>
-      </FormControl>
-      <FormControl {...register("password")} id="password">
-        <FormControl.Layout>
-          <FormControl.Label>비밀번호</FormControl.Label>
-          <FormControl.Password placeholder="비밀번호를 입력해주세요" />
-          <FormControl.ErrorMessage />
-        </FormControl.Layout>
-      </FormControl>
-      <FormControl {...register("passwordConfirm")} id="passwordConfirm">
-        <FormControl.Layout>
-          <FormControl.Label>비밀번호 확인</FormControl.Label>
-          <FormControl.Password placeholder="비밀번호를 다시 입력해주세요" />
-          <FormControl.ErrorMessage />
-        </FormControl.Layout>
-      </FormControl>
-
-      <Button
-        className="gradient w-full h-[65px] rounded-lg mt-[20px] disabled:cursor-not-allowed disabled:bg-gray-100 disabled:bg-none"
-        type="submit"
-        disabled={isFormError}
-      >
-        가입하기
-      </Button>
-    </form>
+    <Form<DefaultValues>
+      defaultValues={defaultValues}
+      validate={validate}
+      onSubmit={onSubmit}
+      className="w-[640px] flex flex-col gap-y-10"
+    >
+      {({ register, isFormError }) => (
+        <>
+          <SignupFormEmail register={register} />
+          <SignupFormNickname register={register} />
+          <SignupFormPassword register={register} />
+          <SignupFormPasswordConfirm register={register} />
+          <Button
+            className="gradient w-full h-[65px] rounded-lg mt-[20px] disabled:cursor-not-allowed disabled:bg-gray-100 disabled:bg-none"
+            type="submit"
+            disabled={isFormError}
+          >
+            가입하기
+          </Button>
+        </>
+      )}
+    </Form>
   )
 }
